7de234f473f77a0479cf6bf5f434091faa251ed8
Kdamzy <kolajidedamilare@gmail.com>
2024-02-07 13:21:38 +0100

    base model

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory"""
+"""__init__ method for models directory"""
 from models.engine.file_storage import FileStorage
 
-"""
-creating an instance of the filestorage to be used
-for our storage.
-"""
 storage = FileStorage()
-"""
- reload Deserializes back to dictionary from json format
-"""
+"""reload back to dictionary from json format"""
 storage.reload()
